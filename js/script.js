c334e646f67dcada28b752ea8fa4758123d62215
Faishal Danni <faishaldanni24@gmail.com>
2024-07-03 09:23:03 +0700

    Adding responsive web and banner

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//variabel ini tidak disimpan permanen
+//Variabel ini tidak disimpan permanen
 var user_name = prompt("Please enter your name : ");
 
 //Di bawah if/else buat menampilkan nama pengunjung di website kalau tidak ada namanya, maka default ke user
@@ -7,6 +7,29 @@ if (user_name == ""){
 }
 else{
     document.getElementById("username").innerHTML = user_name
+}
+
+//Kode untuk menampilkan banner
+var slideIndex = 1;
+showDivs(slideIndex)
+
+//Untuk banner ke-n, hanya bisa 1 atau -1 (Tidak ada banner yang diskip ketika tombol dipencet)
+function plusDivs(n){
+    showDivs(slideIndex += n)
+}
+
+function showDivs(n){
+    var i;
+    var imgList = document.getElementsByClassName("banner-photo");
+    if (n > imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length
+
+    //for loop untuk menghilangkan banner lain kecuali 1
+    for (i = 0; i < imgList.length; i++){
+        imgList[i].style.display = "none";
+    }
+    //-1 ada dibawah karena index dihitung mulai dari 0,1,2 bukan 1,2,3
+    imgList[slideIndex - 1].style.display = "block";
 }
 
 //Di bawah ini untuk validasi form html
